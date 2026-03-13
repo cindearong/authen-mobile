c27f3f79779a87e9b97dc325d9782d7623f9c1cf
cindearong <xinghui@veecotech.com>
2026-03-13 11:04:09 +0800

    Change google API from personal to company google API

--- util/location.js
+++ util/location.js
@@ -1,6 +1,8 @@
 
 
-const GOOGLE_API_KEY = 'AIzaSyAipxg8qHxTFQ-FIY_vcHUI66cSE8FLMeg'
+//const GOOGLE_API_KEY = 'AIzaSyAipxg8qHxTFQ-FIY_vcHUI66cSE8FLMeg'
+
+const GOOGLE_API_KEY = 'AIzaSyC8w8Yi6FHz2PyYAUJvc0RCpFMOY10N3P0'
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
@@ -16,6 +18,9 @@ export async function getAddress(lat, lng) {
   }
 
 const data = await response.json();
+// ADD THE DEBUG LOG HERE
+  console.log("GOOGLE RESPONSE STATUS:", data.status); 
+  console.log("FULL GOOGLE DATA:", data);
 const address = data.results[0].formatted_address;
 return address;
 
